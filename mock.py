9c9846fd799126bd2b10395cc398b2783e46285b
Jim <jimzhu0602@gmail.com>
2023-01-29 21:40:13 +1100

    Good

--- mock.py
+++ mock.py
@@ -5,6 +5,15 @@ import plotly.express as px
 
 app = Dash()
 
+# Methodology for quoting a market:
+'''
+1. Work out fair, using my theo (assuming market is accurate)
+2. Consider width of stock market + relative size/delta of quote to add edge to each size. *Impact + Holding*
+3. Fair + Edge
+
+Being hit/lifted always implies stock = theo +/- edge
+'''
+
 def generate_table(dataframe, last_px, revcon, max_rows=10):
     return html.Table([
         html.Thead(
@@ -15,7 +24,7 @@ def generate_table(dataframe, last_px, revcon, max_rows=10):
                 html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
             ]) for i in range(min(len(dataframe), max_rows))
         ])
-    ], style={'marginLeft': 'auto', 'marginRight': 'auto', 'minWidth': '300px', 'width': '300px', 'maxWidth': '300px'})
+    ], style={'marginLeft': 'auto', 'marginRight': 'auto', 'minWidth': '250px', 'width': '250px', 'maxWidth': '250px'})
 
 def create_board():
     opt = ["_____"]*5
@@ -48,9 +57,10 @@ app.layout = html.Div(style={'backgroundColor': 'white', 'textAlign': 'center'},
     html.H1(children='Hello Mock',),
 
     # Board
-    html.Div(id="board-rc", children=f'r/c = {revcon}' ),
-    html.H4(id="board-title", children=f'Stock = {stock}'),
-    html.H4(id="board", children=[generate_table(df, stock, revcon)]),
+    html.Div(id="board-rc", children=f'r/c = {revcon}', style={'marginBottom': '0px','marginTop': '0px', 'fontSize':12}),
+    html.Div(id="board-title", children=f'Stock = {stock}', style={'marginTop': '5px','margin-bottom': '5px','font-weight': 'bold'}),
+    html.Div(children=f'50 x 50', style={'marginBottom': '0px','marginTop': '0px', 'fontSize':12}),
+    html.Div(id="board", children=[generate_table(df, stock, revcon)], style={'marginTop': '5px','margin-bottom': '5px','font-weight': 'bold'}),
 
     # Generate order
     html.Button('New Order', id='order', n_clicks=0),
@@ -89,21 +99,30 @@ def update_board(n):
 )
 def random_order(n_clicks):
     global imp
+    size = np.random.randint(1,7) * 50
+    # Can be either a combo order or a request a quote
+    r = np.random.randint(2)
+    if r:
+        n=np.random.uniform(-width-0.1,width+0.1)
+        imp=stock + n
 
-    n=np.random.uniform(-width-0.1,width+0.1)
-    imp=stock + n
+        # strike
+        i = np.random.randint(0,5)
+        k=strikes[i]
+        combo_type='puts-over' if k>stock else ''
+        combo_val= np.abs(imp-k+revcon)
 
-    # strike
-    i = np.random.randint(0,5)
-    k=strikes[i]
-    combo_type='puts-over' if k>stock else ''
-    combo_val= np.abs(imp-k+revcon)
-
-    # type
-    i = np.random.randint(0,2)
-    otyp='bids' if i else 'offers'
-    string=f'Customer {otyp} {combo_val:.2f} for the {k} combo'
-
+        # type
+        i = np.random.randint(0,2)
+        otyp='bids' if i else 'offers'
+        string=f'Customer {otyp} {combo_val:.2f} for the {k} combo, {size}x'
+    else:
+        # strike
+        i = np.random.randint(0,5)
+        k=strikes[i]
+        string=f'Give a quote in the {k} combo, {size}x'
+        imp=np.abs(stock-k+revcon)
+        
     return string, f'{imp:.2f}'
 
 @app.callback(Output('boxes', 'style'),[Input('vbutton1', 'n_clicks')])
